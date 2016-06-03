6b66be1c2de9d9dcf1408d5570c864bbf39821cb
bella <15951817767@163.com>
2016-06-03 09:05:56 +0800

    Update zhihu.py
    add comments

--- zhihu.py
+++ zhihu.py
@@ -16,9 +16,14 @@ time.sleep(2)
 
 browser.find_element_by_class_name("switch-to-login").click()
 time.sleep(3)
+"""
+@输入用户名和密码
+"""
 browser.find_element_by_xpath('//input[@aria-label="手机号或邮箱" and @name="account"]').send_keys(account)
 browser.find_element_by_xpath('//input[@placeholder="密码" and  @aria-label="密码"]').send_keys(password)
-#看页面要不要输入验证码，要求输入就去掉注释
+"""
+@看页面要不要输入验证码，要求输入就去掉注释
+"""
 #captcha=raw_input()
 #browser.find_element_by_xpath('//form[@method="POST"]/div[@class="input-wrapper captcha-module"]//input[@id="captcha" and @name="captcha"]').send_keys(captcha)
 browser.find_element_by_xpath('//input[@value="登录" and @class="submit zg-btn-blue"]').submit()
@@ -32,6 +37,9 @@ for i in range(len(expands)):
     contentbr=webdriver.Firefox()
     contentbr.get(url)
     time.sleep(2)
+    """
+    @打开问题页
+    """
     content=contentbr.find_element_by_xpath('//div[@class="zm-editable-content clearfix"]')
     print "赞同回答的内容: ",content.text,"\n"
     contentbr.quit()
